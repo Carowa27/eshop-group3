ac60d0603e35d76bb02ab8a7ba38a9153bb165b9
Carolina <carolina.warntorp@outlook.com>
2025-12-27 22:11:54 +0100

    add start ls functionality to cart

--- JavaScript/local-storage-checkout-fn.js
+++ JavaScript/local-storage-checkout-fn.js
@@ -1,3 +1,28 @@
 // save product in local storage when pressing buy
+export const saveToLSCart = (newItemIndex) => {
+  const newItem = Object.values(products)[newItemIndex];
+  console.log("ls", getLSCart());
+  const LSCart = getLSCart();
+  const updateLSCart = () => {
+    if ((LSCart == undefined) | (LSCart == null)) {
+      console.log("create cart", [{ product: newItem, amount: 1 }]);
+      localStorage.setItem(
+        "CC-Cart",
+        JSON.stringify([{ product: newItem, amount: 1 }])
+      );
+    } else {
+      LSCart.map((prod, i) => {
+        if (prod.product.name == newItem.name) {
+          console.log("it exists, add amount");
+        } else {
+          console.log("does not exist, add prod");
+        }
+      });
+    }
+  };
+  updateLSCart();
+};
 
-// get all products added to checkout
+export const getLSCart = () => {
+  return JSON.parse(localStorage.getItem("CC-Cart"));
+};
