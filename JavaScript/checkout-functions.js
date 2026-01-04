8ce11de3413c978a2d541109d0602a5c9aa97d33
Erica <erica.friberg89@gmail.com>
2026-01-04 11:54:29 +0100

    form updates, small bugfix

--- JavaScript/checkout-functions.js
+++ JavaScript/checkout-functions.js
@@ -6,10 +6,9 @@ function validityCheck() {
     event.target.reportValidity();
   }
 }
-
 //Format card number automatically
 function formatCardNumber() {
-  var cardNumber = document.getElementById("cardnumber");
+  var cardNumber = document.getElementById("card-number");
   var value = cardNumber.value.replace(/\D/g, '');
   var formattedValue = "";
   for (var i = 0; i < value.length; i++) {
@@ -21,6 +20,7 @@ function formatCardNumber() {
   cardNumber.value = formattedValue;
 }
 
+//Expander for mobile view
 const expanderElement = document.getElementById("products");
 const iconElement = document.getElementById("icon-span");
 function expand() {
@@ -40,13 +40,10 @@ window.addEventListener('resize', changeSize);
 function changeSize(){
    if (window.innerWidth > 750) {
       expanderElement.style.display = "block";
-   } else if (window.innerWidth < 750) {
-      expanderElement.style.display = "none";
-      iconElement.classList.add('fa-chevron-down');
-      iconElement.classList.remove('fa-chevron-up');
    } else {
       expanderElement.style.display = "none";
       iconElement.classList.add('fa-chevron-down');
       iconElement.classList.remove('fa-chevron-up');
    }
 }
+
